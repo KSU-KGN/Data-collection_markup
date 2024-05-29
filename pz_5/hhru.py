62914e8b11437e5d3b8ff06747a54dd917eed299
KSU-KGN <61096790+KSU-KGN@users.noreply.github.com>
2024-05-29 23:44:08 +0500

    Add files via upload

--- pz_5/hhru.py
+++ pz_5/hhru.py
@@ -8,10 +8,20 @@ class HhruSpider(scrapy.Spider):
 
     def parse(self, response:HtmlResponse):
         # name - response.xpath("//span[@data-qa = 'serp-item__title']")
+        next_page = response.xpath("//a[@data-qa = 'pager-next']/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
         links = response.xpath("///span[@class = 'serp-item__title-link-wrapper']//@href").getall()
         for link in links:
-            response.follow(link)
-        
+            yield response.follow(link, callback=self.vacancy_parse)
+
+
+    def vacancy_parse(self, response:HtmlResponse):
+        name = response.xpath("//h1/text()").get()
+        salary = response.xpath(("//div[@data-qa = 'vacancy-salary']//text()")).getall()
+        url = response.url
+
         print(response.status, response.url)
         ### pass
 
