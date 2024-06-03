9b3c7ec21cb617c3924d3bec32f15c39078f09de
KSU-KGN <61096790+KSU-KGN@users.noreply.github.com>
2024-06-03 21:40:03 +0500

    Выполнение практического задания № 6
    Выполнение практического задания № 6
    
    items.py	- элемент (Item) в Scrapy
    pict.csv	- дополнительные сведения об изображениях

--- pz_6/items.py
+++ pz_6/items.py
@@ -4,36 +4,19 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 """
-Определите элемент (Item) в Scrapy, который будет представлять изображение.
-Ваш элемент должен включать такие детали,
-как URL изображения, название изображения и категорию, к которой оно принадлежит.
+Каждая строка должна соответствовать одному изображению и содержать URL изображения,
+локальный путь к файлу (после загрузки), название и категорию.
 """
-
 import scrapy
 
 from itemloaders.processors import TakeFirst, MapCompose, Compose
 
-def process_name(value):
-    value = value[0].strip()
-    return value
-
-def process_category(value):
-    value = value[0].strip()
-    return value
-
-def process_photos(value:str):
-    print()
-    if value.startswith('//'):
-        value = 'https:' + value.split()[0]
-    else:
-        value = value.split()[1]
-    return value
-
 class UnsplashparserItem(scrapy.Item):
     # define the fields for your item here like:
-    name = scrapy.Field(input_processor=Compose(process_name), output_processor=TakeFirst())
-    category = scrapy.Field(input_processor=MapCompose(process_category))
-    url = scrapy.Field(output_processor=TakeFirst())
-    photos = scrapy.Field(input_processor=MapCompose(process_photos))
+    # name = scrapy.Field()
+    name = scrapy.Field(output_processor=TakeFirst())       # название фото
+    category = scrapy.Field()                               # список категорий фото
+    url = scrapy.Field(output_processor=TakeFirst())        # URL страницы фото
+    photo = scrapy.Field(output_processor=TakeFirst())      # локальный путь к файлу (после загрузки)
     _id = scrapy.Field()
-
+    print()
